30978be5e6dc4686133076328db0fa6deac0ac92
NaniSkinner <skinner.galvez@gmail.com>
2025-09-08 14:15:41 -0500

    completed setup for project

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import Narvbar from "../components/Narvbar";
+import { resumes } from "../constants";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Resumind" },
@@ -9,5 +9,21 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className=" bg-[url('/images/bg-main.svg')] bg-cover">
+      <Narvbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your resume with Resumind</h1>
+          <h2> Get smart feedback for your resume</h2>
+        </div>
+      </section>
+
+      {resumes.map((resume) => (
+        <div key={resume.id}>
+          <h1> {resume.jobTitle} </h1>
+        </div>
+      ))}
+    </main>
+  );
 }
